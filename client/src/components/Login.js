5d8b1c919ede5633cfe1a36269262d077c56cf70
Janessa Garrow <janessa.garrow@gmail.com>
2020-02-22 10:22:43 -0700

    Create login form with axiosWithAuth to handle login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+    // make a post request to retrieve a token from the api
+    // when you have handled the token, navigate to the BubblePage route
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        axiosWithAuth()
+            .post("/login", user)
+            .then(res => {
+                localStorage.setItem("token", res.data.payload);
+
+                props.history.push("/bubbles");
+            })
+            .catch(err => console.log("Error logging in: ", err));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+                id="username"
+                name="username"
+                type="text"
+                value={user.username}
+                onChange={handleChange}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+                id="password"
+                name="password"
+                type="password"
+                value={user.password}
+                onChange={handleChange}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
 };
 
 export default Login;
