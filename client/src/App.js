87d9bffd5dd22c892b651898db365e07a12ceec1
Janessa Garrow <janessa.garrow@gmail.com>
2020-02-24 20:37:54 -0700

    Fix props in private route

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-                <PrivateRoute exact path="/bubbles" component={BubblePage} />
+                <PrivateRoute path="/bubbles" component={BubblePage} />
             </div>
         </Router>
     );
